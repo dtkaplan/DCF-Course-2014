d33027a061ac6e79f0543e6833f2d82d58a6eced
dtkaplan <dtkaplan@gmail.com>
2015-01-28 16:20:22 -0600

    installation script for Week-1

--- Notes/Week-1/install_packages.R
+++ Notes/Week-1/install_packages.R
@@ -1,6 +1,6 @@
 install.packages("devtools")
 library(devtools)
-devtools::install_github("dtkaplan/DCF")
+devtools::install_github("dtkaplan/DCFdevel")
 devtools::install_github("dtkaplan/DCFinteractive")
 install.packages(c("knitr","rmarkdown","rpart","shiny","manipulate"))
 install.packages(c("mosaic","mosaicData","NHANES"))
