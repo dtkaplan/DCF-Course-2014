f2bc9327d2698cd9a2c245ae2ebc6d53a84435f1
dtkaplan <dtkaplan@gmail.com>
2015-01-28 15:48:51 -0600

    Materials for Week 1

---
+++ Notes/Week-1/install_packages.R
@@ -0,0 +1,7 @@
+install.packages("devtools")
+library(devtools)
+devtools::install_github("dtkaplan/DCF")
+devtools::install_github("dtkaplan/DCFinteractive")
+install.packages(c("knitr","rmarkdown","rpart","shiny","manipulate"))
+install.packages(c("mosaic","mosaicData","NHANES"))
+
